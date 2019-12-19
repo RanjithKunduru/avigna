f28b2b1e78de55dfa84b4790661d8a29c6220091
Ranjith Kumar Kunduru <RanjithKumar_Kunduru@ad.infosys.com>
2019-12-19 16:00:16 +0530

    lamda

--- src/main/java/com/avigna/services/TryAvigna.java
+++ src/main/java/com/avigna/services/TryAvigna.java
@@ -2,6 +2,11 @@ package com.avigna.services;
 
 public class TryAvigna {
 	
+	public void avignasya()
+	{
+		System.out.println("Avignamastu!");
+	}
+	
 	public static void main(String[] ar)
 	{
 		System.out.println("Hello World!");
